d77550efff4f9d84f0127c3e89da1774a2e74950
licl <licl@caibab.com>
2019-05-31 08:39:05 +0800

    梳理整个架构，抽出公共登录模块等

--- pccaibab/runners/demoRunner.py
+++ pccaibab/runners/demoRunner.py
@@ -3,18 +3,20 @@ sys.path.append('../')
 import unittest
 from testcases import demoCase
 from utils import HTMLTestReportCN
+import datetime
 
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     test_cases = [demoCase.DemoTestCase("test_clickSomething"),demoCase.DemoTestCase("test_clickOneMorething")]
     suite.addTests(test_cases)
-    filePath = 'F:\\HTMLTestReportCN.html'
+    fileEnd = datetime.datetime.now().strftime('%Y%m%d%H%M')
+    filePath = 'F:\\HTMLTestReportCN'+fileEnd+'.html'
     fp = open(filePath, 'wb')
     # 生成报告的Title,描述
     runner = HTMLTestReportCN.HTMLTestRunner(
         stream=fp,
-        title='自动化测试报告',
+        title='自动化测试报告'+fileEnd,
         # description='详细测试用例结果',
         tester='Findyou'
     )
